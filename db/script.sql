82ecafa8ca232a4023b7492b0bbc02140c3d76fb
Isabela alcantara <belaalcantara0704@gmail.com>
2024-10-07 07:56:38 -0300

    feat: add inserts testes

--- db/script.sql
+++ db/script.sql
@@ -39,3 +39,23 @@ CREATE TABLE cursos (
     id_area INT REFERENCES area(id_area) ON DELETE CASCADE,
     id_palavraChave INT REFERENCES palavras_chaves(id_palavrasChaves) ON DELETE CASCADE
 );
+
+INSERT INTO cursos (titulo, modalidade, carga_horaria, nivel, descricao, descricao_requisitos, programacao, modalidade_aula, metodologia_ensino, idade, turnos, status, imagem)
+VALUES 
+('Curso de Desenvolvimento Mobile', 'Online', 200, 'Avançado', 'Curso voltado para o desenvolvimento de aplicativos móveis.', 'Conhecimentos intermediários em programação.', 'Kotlin, Swift, React Native', 'Gravada', 'Aulas práticas e teóricas', 20, 'Noite', 'Ativo', 'imagem6.jpg');
+
+INSERT INTO cursos (titulo, modalidade, carga_horaria, nivel, descricao, descricao_requisitos, programacao, modalidade_aula, metodologia_ensino, idade, turnos, status, imagem)
+VALUES 
+('Curso de Data Science', 'Online', 180, 'Avançado', 'Curso completo de ciência de dados.', 'Experiência com programação e análise de dados.', 'Python, SQL, Machine Learning, Data Visualization', 'Ao vivo', 'Aulas interativas com projetos reais', 25, 'Manhã', 'Ativo', 'imagem7.jpg');
+
+INSERT INTO cursos (titulo, modalidade, carga_horaria, nivel, descricao, descricao_requisitos, programacao, modalidade_aula, metodologia_ensino, idade, turnos, status, imagem)
+VALUES 
+('Curso de Segurança da Informação', 'Presencial', 160, 'Intermediário', 'Curso focado em estratégias de segurança digital.', 'Noções básicas de redes de computadores.', 'Firewall, Criptografia, Ethical Hacking', 'Presencial', 'Laboratório prático e teoria', 23, 'Tarde', 'Ativo', 'imagem8.jpg');
+
+INSERT INTO cursos (titulo, modalidade, carga_horaria, nivel, descricao, descricao_requisitos, programacao, modalidade_aula, metodologia_ensino, idade, turnos, status, imagem)
+VALUES 
+('Curso de UX/UI Design', 'Online', 120, 'Iniciante', 'Curso introdutório sobre design de experiência do usuário.', 'Nenhuma experiência necessária.', 'Figma, Adobe XD, Design Thinking', 'Gravada', 'Aulas teóricas e projetos práticos', 18, 'Noite', 'Ativo', 'imagem9.jpg');
+
+INSERT INTO cursos (titulo, modalidade, carga_horaria, nivel, descricao, descricao_requisitos, programacao, modalidade_aula, metodologia_ensino, idade, turnos, status, imagem)
+VALUES 
+('Curso de E-commerce', 'Online', 100, 'Intermediário', 'Curso completo sobre como criar e gerenciar lojas online.', 'Conhecimento básico de marketing digital.', 'Shopify, Woocommerce, Estratégias de Venda', 'Ao vivo', 'Estudos de caso e workshops práticos', 22, 'Manhã', 'Ativo', 'imagem10.jpg');
